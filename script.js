c93a9bfe82d70365c72e9dfd3e305dfab0309956
Kotobaba <ttikttokai@gmail.com>
2025-12-30 18:56:09 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function goToApp() {
+  document.body.style.opacity = "0";
+  setTimeout(() => {
+    window.location.href = "https://huggingface.co/spaces/Kotobaba/audio-transcription-whisper";
+  }, 350);
+}
